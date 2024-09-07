bc810c5863a57e5952e4db771f457c6aa536eab3
NovaCode986m <a.bowker94@outlook.com>
2024-09-07 16:32:52 +0100

    Implemented weather API

--- cinepulse/src/App.tsx
+++ cinepulse/src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [weatherNow, setWeatherNow] = useState<any>(null);
+  const [weatherCached, setWeatherCached] = useState<any>(null)
+  useEffect(() => {
+    const cachedWeather = localStorage.getItem('weatherData');
+    const options = {method: 'GET', headers: {accept: 'application/json'}};
+    if(!cachedWeather){
+      fetch('http://api.weatherapi.com/v1/search.json?key=2332dfb52a52492ab80143318240709&q=Isle of Man&aqi=no', options)
+      .then(response => response.json())
+      .then((response) => {
+        console.log(response)
+        setWeatherNow(response)
+        localStorage.setItem('weatherData', JSON.stringify(response));
+      })
+      .catch(err => console.error(err));
+    }else{
+      setWeatherCached("Weather is cached")
+      setWeatherNow(JSON.parse(cachedWeather))
+    }
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Cinepulse</h1>
+        {weatherCached == "Weather is cached" ? (<p>Weather is cached</p>): (<p>Weather not cached</p>)}
+        {weatherNow ? 
+        ( <p>Current temperature in {weatherNow.location.name}: {weatherNow.current.temp_c}°C</p> ) : 
+        ( <p>Loading weather...</p> )}
       </header>
     </div>
   );
